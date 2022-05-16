8081bde9c28b2fe53a1bd223e1eab88524b4e619
Mayowa Oluwasina <mayowaoluwasina@gmail.com>
2022-05-16 08:07:10 +0100

    source done

--- src/graphql/swapi/index.ts
+++ src/graphql/swapi/index.ts
@@ -1,5 +1,5 @@
 import swapiResolvers from "./resolvers";
 import swapiTypeDefs from "./typeDef";
-import StarWarAPI from "./startwar-api";
+import StarWarAPI from "./source";
 
 export {swapiResolvers, swapiTypeDefs, StarWarAPI};
